51484a34a15d87482e34c10a682d5923a1f60b98
Nyabayo <ernestosindo9@gmail.com>
2024-11-06 01:02:52 +0300

    Day2

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
