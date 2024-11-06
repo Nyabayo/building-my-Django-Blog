2f6eef0f7601ffd531d06d66f4a140ada113c4e4
Nyabayo <ernestosindo9@gmail.com>
2024-11-07 00:35:46 +0300

    Day3

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import BlogPost
 
-# Register your models here.
-admin.site.register(BlogPost)
+# Customize the BlogPost display in the admin panel
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'updated_date')
+    list_filter = ('created_date', 'author')
+    search_fields = ('title', 'content')
 
+# Register the BlogPost model with the customized admin class
+admin.site.register(BlogPost, BlogPostAdmin)
