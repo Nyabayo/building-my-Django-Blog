80ce5f7804e5f28ce7711d21a4eae0a21de4760b
Nyabayo <ernestosindo9@gmail.com>
2024-11-07 23:49:37 +0300

    Day4

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -13,4 +11,3 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
-
