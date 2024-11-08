8e78f9e6b037b83496aad1c534f7b9df012c3080
Nyabayo <ernestosindo9@gmail.com>
2024-11-08 23:44:59 +0300

    Day5

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),  # Homepage
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),  # Detailed post page
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 def post_list(request):
     posts = BlogPost.objects.all().order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
